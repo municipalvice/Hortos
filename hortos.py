cccee58b8d8a4efacd1c89b7ceb7f4e572e4722f
Abraham Herrera <50112365+municipalvice@users.noreply.github.com>
2021-02-23 22:32:05 -0600

    added scheduler to get_weather call

--- hortos.py
+++ hortos.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
-
-from record import record
+import time, sched
 from weather import get_weather
 
-try:
-    # Get weather reading
+scheduler = sched.scheduler(time.time, time.sleep)
+
+def schdule_reading(sc):
     weather = get_weather()
-    # Record weather reading
-    record(weather)    
+    humidity = weather['humidity']
+    if (humidity < 55.0) {
+        humidifier_on()
+    } elif (humidity > 85.0) {
+        humidifier_off()
+    }
+    scheduler.enter(6, 1, schdule_reading, (sc,))
+
+try:
+    scheduler.enter(6, 1, schdule_reading, (scheduler, ))
+    scheduler.run()
 except:
     print("Unable to record weather...")
 
